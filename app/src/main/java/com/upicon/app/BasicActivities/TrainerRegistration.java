9d98ace85a7357f6aa8952dd6add59482ef98469
Bhumikasaxena16 <bhumikasaxena1616@gmail.com>
2023-06-13 15:53:59 +0530

    Updated Code

---
+++ app/src/main/java/com/upicon/app/BasicActivities/TrainerRegistration.java
@@ -0,0 +1,16 @@
+package com.upicon.app.BasicActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.upicon.app.R;
+
+public class TrainerRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trainer_registration);
+    }
+}
---
+++ app/src/main/java/com/upicon/app/MSDashboard.java
@@ -0,0 +1,14 @@
+package com.upicon.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MSDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_msdashboard);
+    }
+}
